c4cee67d1a8f9a4daf3468e02308d1fc1f557370
mmasso <mmasso@cifpfbmoll.eu>
2020-05-17 15:25:41 +0200

    docs(CrystalExpender): Javadoc Implementation

--- src/main/java/edu/elsmancs/ricksybusiness/CrystalExpender.java
+++ src/main/java/edu/elsmancs/ricksybusiness/CrystalExpender.java
@@ -1,15 +1,30 @@
 package edu.elsmancs.ricksybusiness;
 
+/**
+ * Clase para vender los cristales a sus compradores
+ * @author Mateu Massó
+ */
 public class CrystalExpender implements GuestDispatcher {
 
     private int stock = 0;
     private double itemPrice = 0;
 
+    /**
+     * Constructor de la clase Crystal expender
+     * @param stock le indicamos la cantidad de cristales que la tienda podra vender
+     * @param price le indicamos el precio unitario del cristal
+     */
 	public CrystalExpender(int stock, double price) {
         this.stock = stock;
         this.itemPrice = price;
 	}
 
+    /**
+     * Método usado para assignar a cada nuevo cliente, un cristal.
+     * 
+     * @throws Exception Cuando la creditCard no cumple los requisitos lanza exception
+     * @param creditCard Tarjeta de credito identificativa de cada cliente.
+     */
     public void dispatch(CreditCard creditCard) {
         try {
             // comprueba si hay credito //
@@ -22,6 +37,11 @@ public class CrystalExpender implements GuestDispatcher {
         }
     }
 
+    /**
+     * Método requerido para controlar las excepciones que pueda lanzar el método dispatch
+     * @param holds boolean para comprobar lanzar la excepción
+     * @throws Exception
+     */
     private void require(Boolean holds) throws Exception {
         if (!holds) {
             throw new Exception();
@@ -29,12 +49,15 @@ public class CrystalExpender implements GuestDispatcher {
     }
 
     /**
-     * @return the stock
+     * @return devuelve el stock de cristales
      */
     public int stock() {
         return stock;
     }
 
+    /**
+     * Overdrive del toString() base para imprimir el stock y el precio unitario de los cristales
+     */
     @Override
     public String toString() {
         return "stock: " + stock + "\n" +
